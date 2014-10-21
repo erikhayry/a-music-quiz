ad5599aef5cb45e1d4a3033325ad798da5e0b2cd
Erik Portin <erikportin@gmail.com>
2014-10-21 11:51:21 +0100

    reinstalled karma

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,25 +37,68 @@ module.exports = function(grunt) {
                 configFile: 'karma.conf.js',
                 options: {
                     files: [
-                            'test/utilities/**/*.js',
-                            //'test_libs/react-with-addons.js',
-                            //'app/build_jsx/**/*.js',        
+                            'test/utilities/**/*.js',       
+                            'scripts/vendor/q.js',
                             'app/scripts/**/*.js',
-                            'test/**/*.js',
-                            'scripts/vendor/q.js'
+                            'test/**/*.js'
                         ],
                     exclude: [
                             'test/jsx/**/*.js',
                             'app/scripts/app.js'
                     ]    
                 }
+            },
+            react: {
+                configFile: 'karma.conf.js',
+                options: {
+                    files: [
+                            'test/utilities/**/*.js',
+                            'test_libs/react-with-addons.js',
+                            'scripts/vendor/q.js',
+                            'app/build_jsx/**/*.js',        
+                            'app/scripts/**/*.js',
+                            'test/jsx/**/*.js'                            
+                        ] 
+                }
+            },
+            file: {
+                configFile: 'karma.conf.js',
+                options: {
+                    files: [
+                            'test/utilities/**/*.js',
+                            'test_libs/react-with-addons.js',
+                            'scripts/vendor/q.js',
+                            '<%= path %>'
+                        ] 
+                }                
             }
         },
      
     });
 
+    grunt.registerTask('test', function(type){
+        console.log(type)
+        if(type){
+            switch(type){
+                case 'react':
+                    grunt.task.run('build', 'karma:react')
+                    break;
+                case 'unit':
+                    grunt.task.run('karma:unit')
+                    break;                
+                default:
+                    grunt.config('path', type);
+                    grunt.task.run('build', 'karma:file')
+                    break;
+            }            
+        }
+        else{
+            grunt.task.run('build', 'karma:react', 'karma:unit');
+        }
+    })
+
     grunt.registerTask('serve', ['watch:jsx']);
     grunt.registerTask('build', ['react']);
-    grunt.registerTask('test', ['build', 'karma']);
+    //grunt.registerTask('test', ['build', 'karma:unit', 'karma:react']);
 
 };
